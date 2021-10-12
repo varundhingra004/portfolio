1d28ac5b38cfee8b1244f452f8425fdef4a48dff
varundhingra004 <varundhingra004@gmail.com>
2021-10-12 18:59:34 -0400

    Added installation event

--- sw.js
+++ sw.js
@@ -10,6 +10,14 @@ var filesToCache = [
     'images/logo.png'
 ]
 
+self.addEventListener('install', function(e){
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache){
+        return cache.addAll(filesToCache);
+        })
+    );
+});
+
 self.addEventListener('fetch', function(e){
     e.respondWith(
         caches.match(e.request).then(function(response){
