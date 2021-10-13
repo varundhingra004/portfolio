1f4ae35c3399d966fc50c1b67f9003ed07295f77
varundhingra004 <varundhingra004@gmail.com>
2021-10-12 22:27:42 -0400

    rwrwer

--- js/main.js
+++ js/main.js
@@ -2,6 +2,11 @@ window.onload = () => {
     'use strict';
 
     if('serviceWorker' in navigator){
-        navigator.serviceWorker.register('./sw.js');
+        navigator.serviceWorker.register('./sw.js').catch(
+            error => {
+                console.log("Service worker registeration Error");
+            }
+        );
     }
+    
 }
